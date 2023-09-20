3a2ecfee5c29ef642bbe4369a53d91a84ecf94ed
Helmi <muhamadkelmi@gmail.com>
2023-09-20 22:56:57 +0700

    update

--- app/Http/Controllers/oAuthController.php
+++ app/Http/Controllers/oAuthController.php
@@ -33,10 +33,10 @@ class oAuthController extends Controller
     public function authorizeTwitter() {         
         $request_token = $this->twitterOAuth->oauth('oauth/request_token', array('oauth_callback' => $this->callback));         
         $connection = new TwitterOAuth($this->consumerKey, $this->consumerSecret, $request_token['oauth_token'], $request_token['oauth_token_secret']);
+        dd($connection);
         $access_token = $connection->oauth("oauth/access_token", [
             "oauth_verifier" => $connection['oauth_verifier']
-        ]);
-        dd($access_token);
+        ]);        
         oAuth::create([
             'user_id' => auth()->id(),
             'provider' => 'twitter',
