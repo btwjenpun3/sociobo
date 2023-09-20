6466ff17704b025d4469849fc1b89523eba74586
Helmi <muhamadkelmi@gmail.com>
2023-09-21 00:19:23 +0700

    update

--- app/Http/Controllers/Social/TwitterController.php
+++ app/Http/Controllers/Social/TwitterController.php
@@ -6,17 +6,39 @@ use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 use App\Models\User;
 use App\Models\oAuth;
+use App\Http\Controllers\oAuthController;
 use Illuminate\Support\Facades\Http;
 use Abraham\TwitterOAuth\TwitterOAuth;
 
 class TwitterController extends Controller
-{      
+{    
+    private $consumerKey;
+    private $consumerSecret;
+    private $twitterOAuth;
+    private $callback;
+    private $connection;
+    private $userAccessToken;
+
+    public function __construct() {
+        $this->consumerKey = env('TWITTER_CONSUMER_KEY');
+        $this->consumerSecret = env('TWITTER_CONSUMER_SECRET');
+        $this->callback = env('TWITTER_CALLBACK_URL');
+        $this->twitterOAuth = new TwitterOAuth(
+            $this->consumerKey, 
+            $this->consumerSecret
+        );
+        $this->userAccessToken = oAuth::where('user_id', auth()->id())->first();
+        $this->connection = new TwitterOAuth($this->consumerKey, $this->consumerSecret, $this->userAccessToken->oauth_token, $this->userAccessToken->oauth_token_secret);
+    }
+
     public function index() {        
-        $userData = oAuth::where('user_id', auth()->id())->first();
-        if(isset($userData)) {
-            return view('Pages.Social Media.Twitter.index', [
-                'name' => $userData->screen_name,
-            ]);
+        $userData = $this->userAccessToken;
+        if(isset($userData)) {   
+            $user = $this->connection->get('account/verify_credentials', ['tweet_mode' => 'extended', 'include_entities' => 'true']);         
+            // return view('Pages.Social Media.Twitter.index', [
+            //     'name' => $userData->screen_name,
+            // ]);
+            return response()->json($user);
         } else {
             return view('Pages.Social Media.Twitter.index');
         }
